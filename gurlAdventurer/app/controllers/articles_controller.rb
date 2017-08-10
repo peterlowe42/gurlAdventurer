4be5152224ca3df034abcccedec81e2116984133
Pete <Peter Lowe>
2017-08-10 11:46:57 -0700

    add new and create and show routes to articles

--- gurlAdventurer/app/controllers/articles_controller.rb
+++ gurlAdventurer/app/controllers/articles_controller.rb
@@ -1,5 +1,38 @@
 class ArticlesController < ApplicationController
 	def index
+		@articles = Article.all
+	end
+
+	def show
+		@article = Article.find(params[:id])
 		
 	end
+
+	def new
+
+	end
+
+	def edit
+	end
+
+	def create
+		@article = Article.new(params_article)
+		if @article.save
+			redirect_to @article
+		else
+			redirect_to new_article_path
+		end
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+		def params_article
+			params.require(:article).permit(:title, :body, :author)
+		end
 end
