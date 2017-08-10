8894ea569eb7c4c58ec76e7a2f1f8466d9a3e43d
Pete <Peter Lowe>
2017-08-09 18:58:23 -0700

    set up foundation and add articles model

---
+++ gurlAdventurer/db/migrate/20170810015123_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+      t.string :author
+      t.string :youtube_url
+
+      t.timestamps null: false
+    end
+  end
+end
