193c7c3fe744d4d109bad2566b0c5b88091d5907
Pete <Peter Lowe>
2017-08-14 14:04:59 -0700

    Add and instal acts_as_taggable_on gem, add tags to seed data

--- gurlAdventurer/app/models/article.rb
+++ gurlAdventurer/app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ActiveRecord::Base
+	acts_as_taggable
+	has_many :comments 
+
 	has_attached_file :image, styles: {large: "600x450#", medium: "300x225#", thumb: "150x112#" }, default_url: "/images/:style/missing.png"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
--- gurlAdventurer/db/seeds.rb
+++ gurlAdventurer/db/seeds.rb
@@ -6,9 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+tags = ["Scuba", "Travel", "Kayaking", "Outdoors", "Hiking", "Sailing", "Extreame", "Lifestyle", "Sports", "Running"]
+
 20.times do
+	tags_list = tags.shuffle[0, rand(1..5)]
 	Article.create({title:Faker::Hipster.sentence, 
 										body:"#{Faker::Hipster.paragraph(20)}<br><br>#{Faker::Hipster.paragraph(20)}<br><br>#{Faker::Hipster.paragraph(15)}",
 										author:Faker::Hobbit.character,
-										image: File.new("/home/pete/Pictures/tempImgs/#{rand(1..12)}.jpg")})
+										image: File.new("/home/pete/Pictures/tempImgs/#{rand(1..12)}.jpg"),
+										tag_list: tags_list})
 end
