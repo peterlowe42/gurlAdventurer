ae042e86f250a97c9e68b1b9bb61e8e78f150f51
Pete <Peter Lowe>
2017-08-17 20:00:16 -0700

    Add migration for featured boolean

--- gurlAdventurer/db/schema.rb
+++ gurlAdventurer/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814195727) do
+ActiveRecord::Schema.define(version: 20170818025554) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170814195727) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.boolean  "featured"
   end
 
   create_table "comments", force: :cascade do |t|
