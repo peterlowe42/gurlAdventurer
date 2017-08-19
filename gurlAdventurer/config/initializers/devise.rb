2dbac9c36827baded9213adb5c098d4897cb3714
Pete <Peter Lowe>
2017-08-19 10:16:42 -0700

    add username to devise user model

--- gurlAdventurer/config/initializers/devise.rb
+++ gurlAdventurer/config/initializers/devise.rb
@@ -7,7 +7,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = 'eb547313d9247c33659f07a18948be3bfd44a67fecaf6b299126165bf381ba3ad563424ccf66e29593420eeec11e11e8e000c8bdfbe7b9c34f4c9fd61c0442e8'
-
+  config.scoped_views = true
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
