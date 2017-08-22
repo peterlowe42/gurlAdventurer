59f3b909d14147dc33c22ee323bedc98e93dbe54
Pete <Peter Lowe>
2017-08-21 17:25:36 -0700

    Add pictres model and configure admin panel

---
+++ gurlAdventurer/app/models/picture.rb
@@ -0,0 +1,6 @@
+class Picture < ActiveRecord::Base
+	belongs_to :article
+
+	has_attached_file :img, styles: {large: "900x675#", medium: "300x225#"}, default_url: "/images/:style/missing.png"
+	validates_attachment_content_type :img, content_type: /\Aimage\/.*\z/
+end
