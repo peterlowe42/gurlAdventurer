6a26d85e7777b5fa096c0179192ef571eff75f3b
Pete <Peter Lowe>
2017-08-23 12:41:29 -0700

    Add categories to seed data

--- gurlAdventurer/db/seeds.rb
+++ gurlAdventurer/db/seeds.rb
@@ -6,7 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Article.destroy_all
+Category.destory_all
+
 tags = ["Scuba", "Travel", "Kayaking", "Outdoors", "Hiking", "Sailing", "Extreame", "Lifestyle", "Sports", "Running"]
+categories = ["Sports", "Travel", "Outdoors", "Lifestyle"]
+
+categories.each do |cat|
+	Category.create(name: cat)
+end
 
 20.times do
 	tags_list = tags.shuffle[0, rand(1..5)]
@@ -14,8 +22,11 @@ tags = ["Scuba", "Travel", "Kayaking", "Outdoors", "Hiking", "Sailing", "Extream
 										body:"#{Faker::Hipster.paragraph(20)}<br><br>#{Faker::Hipster.paragraph(20)}<br><br>#{Faker::Hipster.paragraph(15)}",
 										author:Faker::Hobbit.character,
 										image: File.new("/home/pete/Pictures/tempImgs/#{rand(1..12)}.jpg"),
-										tag_list: tags_list
+										tag_list: tags_list,
+										category_id: rand(1..4)
 										})
 end
+
+
 
 Article.first.update(featured: true)
