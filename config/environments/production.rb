a346e0dfe9109d699dce112d275c937975f8aa85
Pete <Peter Lowe>
2017-08-30 22:00:17 -0700

    Add gmail setup for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,22 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'https://gurladventures.herokuapp.com/' }
 
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings = 
+  {
+
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :domain             => 'gmail.com',
+    :authentication     => :plain,
+    :user_name          => 'gurladventures@gmail.com',
+    :password           => 'luke42016',
+    :authentication     =>  :login,
+    enable_starttls_auto: true
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
