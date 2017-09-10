715d9669fee42255f78682e185b291a4dffea05a
Pete <Peter Lowe>
2017-09-10 11:51:37 -0700

    Update gemfile.lock

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,6 @@ Rails.application.configure do
     :aaddress            => 'smtp.gmail.com',
     :port               => 587,
     :domain             => 'gmail.com',
-    :authentication     => :plain,
     :user_name          => 'gurladventures@gmail.com',
     :password           => ENV['MAIL_PASSWORD'],
     :authentication     =>  :login,
